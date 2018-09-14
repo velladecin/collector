d0a6dc8891916020045f459b7addf3418fb8f8a6
vella <viktorvillafuerte@nbnco.com.au>
2018-09-14 11:24:25 +1000

    removed the 'modem accumulation' problem by merging only IPs for current modems

--- collector.py
+++ collector.py
@@ -2,16 +2,15 @@
 import os, sys, re
 import pexpect
 import pickle, json
-import time
+import time, signal
 from pprint import pprint
 # T3
 from t3_PyLib import utils
 
 CMTS_BATCH_SIZE = 20
-CMTS_CM_AGEING = 8 * 24 * 60 * 60  # 8 days
 
 CMTSCACHEDIR  = 'cmtscache'
-CMTSCACHEMAXKEEP = 10
+CMTSCACHEIPPOOL = 10
 CMTSLOGDIR = 'cmtslogs'
 BASEDIR = os.path.dirname(sys.argv[0])
 LOG = open('%s.log' % sys.argv[0], 'a', 0)
@@ -20,7 +19,7 @@ LOG.write('*** RUN ***** %s *****************************************\n' % utils
 def log(logfh, msg, lvl='INFO'):
     logfh.write("%s [%s]  %s\n" % (utils.now(), lvl, msg))
 
-def run_child(cmts):
+def run_child(cmts, uname, passw):
     pid = os.getpid()
     basedir = BASEDIR
     if not basedir: basedir = '.'
@@ -29,50 +28,29 @@ def run_child(cmts):
     cmtslog = open('%s/%s/%s.log' % (basedir, CMTSLOGDIR, cmts), 'a', 0)
     log(cmtslog, "Fork() CMTS %s, PID %d" % (cmts, pid))
 
-    uname, passw = utils.get_creds()
-    if not uname or not passw:
-        log(cmtslog, "Failed retrieving uname/passw", 'CRITICAL')
-        sys.exit(1)
-
-    cmtsdict = {}
     cmtscachefile = "%s/%s/%s" % (basedir, CMTSCACHEDIR, cmts)
     cmtscachefilejson = "%s.json" % cmtscachefile
+    cmtscachefilejsontmp = "%s.json.tmp" % cmtscachefile
 
     # We use both pickle + JSON
     # In Python we read-in with pickle but dump both pickle and JSON
+    # Pickle is only used to track last IPs (up to 5) per each CM
     # Reason:
     # 1. make read in python quicker
     # 2. allow for reading of results by Go in JSON - see T3 webservice
+    pickledict = {}
+    finaldict = {} # this is our result
 
     # load existing cache from disk
     if os.path.isfile(cmtscachefile):
         log(cmtslog, "Reading previous collection from file: %s" % cmtscachefile)
         with open(cmtscachefile, "rb") as fh:
-            cmtsdict = pickle.loads(fh.read())
+            pickledict = pickle.loads(fh.read())
 
-    # 'Offline' modems disappear from CMTS after 7 days of inactivity. We want smth similar
-    # to happen here too. This is to avoid indefinite modem growth due to tech's hand held
-    # devices that move around. (also possibly due to the unlikely modem migrations)
-    # Delete any modems older than 8 days.
-
-    # see 'lastseen' below
-    now = int(utils.now('%s'))
-    for mac in cmtsdict.keys():
-        # added feature due to collections having gotten bloated,
-        # so start fresh :)
-        if not "lastseen" in cmtsdict[mac]:
-            del cmtsdict[mac]
-            continue
-
-        if not isinstance(cmtsdict[mac]["lastseen"], int):
-            cmtsdict[mac]["lastseen"] = int(cmtsdict[mac]["lastseen"])
-
-        if (now - cmtsdict[mac]["lastseen"]) > CMTS_CM_AGEING:
-            del cmtsdict[mac]
-            
     cmtsprompt = '%s#' % cmts
     passprompt = 'password:'
 
+    now = int(utils.now('%s'))
     try:
         child = pexpect.spawn('ssh {0}@{1}'.format(uname, cmts), timeout=90)
         res = child.expect( [pexpect.TIMEOUT, 'Are you sure you want to continue connecting', passprompt] );
@@ -82,7 +60,7 @@ def run_child(cmts):
 
         if res == 1: # new CMTS (SSH key)
             child.sendline('yes')
-            res = child.expect( [pexpect.TIMEOUT, pass_prompt] )
+            res = child.expect( [pexpect.TIMEOUT, passprompt] )
             if res == 0:
                 raise Exception("Timeout accepting new SSH key")
 
@@ -98,8 +76,14 @@ def run_child(cmts):
         ##
         ## 1. collect CM details
 
+        # After long testing it seems that the best option is to simply collect what's currently on the CMTS.
+        # Discard anything that has disappeared. This will lose some historical data for eg ppl who are on holidays,
+        # but currently it seems to be the best option. (do I hear.. doh?)
+
         for line in result.split('\r\n'):
             # 5/7/0-3/7/10      48     8x4   Operational 3.0    12M/1240     0  bcca.b5ff.69b5  fde5:c758:f711:2f:fc63:55cf:fa7d:9992
+            # 10/2/8-2/1/12     19     16x4  Operational 3.1    25M/5215     0  7823.aea8.376d  fde5:c758:f711:6512:7d0c:5025:efaf:494f
+            # 10/2/8-2/1/12     19           Offline     3.1                 0  7823.aea8.37b9
             if not re.match('\d+/\d+/\d+\-\d+/\d+/\d+', line):
                 continue
 
@@ -111,26 +95,22 @@ def run_child(cmts):
                 continue
 
             # STATUS
-            #10/2/8-2/1/12     19     16x4  Operational 3.1    25M/5215     0  7823.aea8.376d  fde5:c758:f711:6512:7d0c:5025:efaf:494f
-            #10/2/8-2/1/12     19           Offline     3.1                 0  7823.aea8.37b9
-
-            src = re.search('\d+/\d+/\d+\-\d+/\d+/\d+\s+[0-9]+\s+[0-9]+x[0-9]\s+([a-zA-Z]+)\s+[0-9]\.[0-9]', line)  # line 1 above
+            src = re.search('\d+/\d+/\d+\-\d+/\d+/\d+\s+[0-9]+\s+[0-9]+x[0-9]\s+([a-zA-Z]+)\s+[0-9]\.[0-9]', line)  # line 1, 2 above
             if not src:
-                src = re.search('\d+/\d+/\d+\-\d+/\d+/\d+\s+[0-9]+\s+([a-zA-Z]+)\s+[0-9]\.[0-9]', line)             # line 2 above
+                src = re.search('\d+/\d+/\d+\-\d+/\d+/\d+\s+[0-9]+\s+([a-zA-Z]+)\s+[0-9]\.[0-9]', line)             # line 3 above
 
-            status = "unknown"
             try:
                 status = src.group(1)
             except AttributeError:
-                pass
+                status = "unknown"
 
             # FNODE
             n = re.search('\d+/\d+/\d+\-\d+/\d+/\d+\s+([0-9]+)\s+', line)
-            node = "unknown"
+
             try:
                 node = n.group(1)
             except AttributeError:
-                pass # should not happen
+                node = "unknown" # should not happen
 
             # CHANNELS, PROFILE
             rechn = re.findall('[0-9]+x[0-9]+', line)
@@ -141,49 +121,48 @@ def run_child(cmts):
             chn = "unknown"
             prf = "unknown"
 
-            if reip6:
-                ip6 = reip6[0]
-            if rechn:
-                chn = rechn[0]
-            if reprf:
-                prf = reprf[0]
+            if reip6: ip6 = reip6[0]
+            if rechn: chn = rechn[0]
+            if reprf: prf = reprf[0]
 
-            # If modem not present in collection yet - then it's a new (or returning) modem.
-            # We add 'lastseen' key to keep track as to when it was last seen on a CMTS
+            if not mac in finaldict: # is this really necessary?
+                finaldict[mac] = {}
 
-            if not mac in cmtsdict:
-                cmtsdict[mac] = {}
-                cmtsdict[mac]["ip"] = []
+            finaldict[mac]["status"] = status
+            finaldict[mac]["node"] = node
+            finaldict[mac]["chans"] = chn
+            finaldict[mac]["speed"] = prf
 
-            cmtsdict[mac]["status"] = status
-            cmtsdict[mac]["node"] = node
-            cmtsdict[mac]["chans"] = chn
-            cmtsdict[mac]["speed"] = prf
-            cmtsdict[mac]["lastseen"] = now
+            # get current IP (if any)
+            ips = []
+            if not mac in pickledict:
+                pickledict[mac] = {}
 
-            app = False
+            if "ip" in pickledict[mac]:
+                ips.extend(pickledict[mac]["ip"])
+
+            # action this only:
+            # 1. when current IP available
+            # 2. current IP differs from last recorded one
+
             if ip6:
-                ips = cmtsdict[mac]["ip"]
-
-                # add IP to cache when:
-                # 1. no previous ips recorded yet (eg: first run)
-                # 2. current ip does not match previous
-
-                # TODO
-                # keep times of from when till when IP was in use?
+                ip6 = ip6.rstrip() # strip \r\n
 
                 if ips:
-                    # previous vs current IP
-                    if ips[-1] != ip6:
-                        app = True
-                        # remove index 0 if cache limit hit
-                        if len(ips) >= CMTSCACHEMAXKEEP: # XXX should we chop to size here??
-                            cmtsdict[mac]["ip"] = ips[1:]
-                else:
-                    app = True
+                    if ip6 != ips[-1]: # current vs last
+                        # keep IP pool size
+                        if len(ips) == CMTSCACHEIPPOOL:
+                            ips = ips[1:]
 
-            if app:
-                cmtsdict[mac]["ip"].append(ip6)
+                        ips.append(ip6)
+                else:
+                    ips.append(ip6)
+
+            # assign result
+            finaldict[mac]["ip"] = ips
+            pickledict[mac]["ip"] = ips
+
+        #pprint(finaldict)
 
         ##
         ## 2. collect FW version
@@ -215,29 +194,42 @@ def run_child(cmts):
 
             # this should always succeed
             try:
-                cmtsdict[m]["fw"] = f
+                finaldict[m]["fw"] = f
             except KeyError:
                 continue
 
     except Exception, e:
         log(cmtslog, "CMTS %s failed processing with: %s" % (cmts, str(e)), 'CRITICAL')
 
-    #pprint(cmtsdict)
+    #pprint(pickledict)
+    #pprint(finaldict)
     #sys.exit(0)
 
-    # dump result to file
+    # dump result to files
     log(cmtslog, "Dumping collection result to file (pickle): %s" % cmtscachefile)
     with open(cmtscachefile, "wb") as fh:
-        pickle.dump(cmtsdict, fh)
+        pickle.dump(pickledict, fh)
 
-    log(cmtslog, "Dumping collection result to file (json): %s" % cmtscachefilejson)
-    with open(cmtscachefilejson, "w") as fh:
-        json.dump(cmtsdict, fh)
+    # At times the T3 webservice cannot parse the JSON file (JSON invalid - missing end of file) which is due
+    # to race condition here. Try to avoid it by dumping into a .tmp file and then renaming.
+    log(cmtslog, "Dumping collection result to file (json): %s" % cmtscachefilejsontmp)
+    with open(cmtscachefilejsontmp, "w") as fh:
+        json.dump(finaldict, fh)
+
+    log(cmtslog, "Renaming '%s' to '%s' to finalize collection" % (cmtscachefilejsontmp, cmtscachefilejson))
+    os.rename(cmtscachefilejsontmp, cmtscachefilejson)
 
     cmtslog.close()
     sys.exit(0)
 
 def cmts_collection(allcmts):
+    # Retrieve creds here to allow for creds change mid-flight.
+    # (not to lose the full run)
+    uname, passw = utils.get_creds()
+    if not uname or not passw:
+        log(LOG, "Failed retrieving uname/passw", 'CRITICAL')
+        sys.exit(1)
+
     count = 0
     for cmts in allcmts:
         try:
@@ -248,7 +240,7 @@ def cmts_collection(allcmts):
 
         # child
         if pid == 0:
-            run_child(cmts)
+            run_child(cmts, uname, passw)
 
         log(LOG, "Fork() CMTS %s, PID %d" % (cmts, pid))
         count += 1
@@ -263,6 +255,8 @@ def main(params):
     allcmts = utils.get_cmts_list().keys()
     #allcmts = ['SWCMT0000221']
     #allcmts = ['SWCMT0000019']
+    #allcmts = [ 'SWCMT0000164']
+    #allcmts = [ 'SWCMT0000279' ]
 
     # we assume we have ~300 CMTS in total
     # split them by CMTS_BATCH_SIZE
@@ -286,7 +280,15 @@ def main(params):
 
     return 1
 
+## TODO this needs fixing!!!
+def ctrlc_handler(signum, frame):
+    print "\nCaught CTRL+C, leaving at user's request.."
+    time.sleep(.5)
+    sys.exit(0)
+
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, ctrlc_handler)
+
     try:
         res = main(sys.argv)
         if res == 1:
